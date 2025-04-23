b327e3274f550ea5384bddab0ae238da2cb2cef8
zhaemin <95492807+zhaemin@users.noreply.github.com>
2025-04-23 14:58:08 +0900

    Add files via upload

--- 0403_CDFSL_test/test_models/batchnorm_tuning.py
+++ 0403_CDFSL_test/test_models/batchnorm_tuning.py
@@ -21,11 +21,11 @@ class CustomBatchNorm(nn.Module):
         self.weight = nn.Parameter(torch.ones(num_features))
         self.bias = nn.Parameter(torch.zeros(num_features))
 
-    def forward(self, x, mean=None, var=None):
+    def forward(self, x, mean=None, var=None, weight=1., bias=0.):
         if mean is None:
-            mean = torch.mean(x, dim=0)
+            mean = torch.mean(x, dim=(0,2)).unsqueeze(0).unsqueeze(-1) # 1 L 1
         if var is None:
-            var = torch.var(x, dim=0, unbiased=False)
+            var = torch.var(x, dim=(0,2), unbiased=False).unsqueeze(0).unsqueeze(-1)
         x_norm = (x - mean) / ((var + self.eps) ** 0.5)
         out = self.weight * x_norm + self.bias
         return out, mean, var
@@ -33,34 +33,32 @@ class CustomBatchNorm(nn.Module):
 class BNBlock(vit.Block):
     def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
                  drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, attn_class=vit.Attention):
-        super().__init__(dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
-                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, attn_class=vit.Attention)
-        self.bn1 = CustomBatchNorm(dim)
-        self.bn2 = CustomBatchNorm(dim)
-        
-    def forward(self, x, return_attention=False, prototype_stat=None):
+        super().__init__(dim, num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale, drop=drop, attn_drop=attn_drop,
+                 drop_path=drop_path, act_layer=act_layer, norm_layer=norm_layer, attn_class=attn_class)
+        self.st_bn1 = CustomBatchNorm(dim)
+        self.st_bn2 = CustomBatchNorm(dim)
+    
+    def forward(self, x, return_attention=False, supports_stat=None):
         mean1, var1, mean2, var2 = None, None, None, None
-        if prototype_stat:
-            mean1, var1, mean2, var2 = prototype_stat[0], prototype_stat[1], prototype_stat[2], prototype_stat[3]
+        if supports_stat != None:
+            mean1, var1, mean2, var2 = supports_stat[0], supports_stat[1], supports_stat[2], supports_stat[3]
         
-        x_bn, mean1, var1 = self.bn1(x, mean1, var1)
-        x = x_bn + self.norm1(x)
-        y, attn = self.attn(x)
+        y, attn = self.attn(self.norm1(x))
         if return_attention:
             return attn
         x = x + self.drop_path(y)
-        x_bn, mean2, var2  = self.bn2(x, mean2, var2)
-        x = x_bn + self.norm2(x)
-        x = x + self.drop_path(self.mlp(x))
+        x = x + self.drop_path(self.mlp(self.norm2(x)))
+        
+        x, mean1, var1 = self.st_bn1(x, mean1, var1)
         
         return x, mean1, var1, mean2, var2
-
+    
 
 class VisionTransformer(vit.VisionTransformer):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.st_clsmod = nn.Parameter(torch.zeros_like(self.cls_token))
-        self.prototype_stat = [None for _ in range(len(self.blocks))]
+        self.supports_stat = [None for _ in range(len(self.blocks))]
     
     def prepare_tokens(self, x):
         B, nc, w, h = x.shape
@@ -76,19 +74,20 @@ class VisionTransformer(vit.VisionTransformer):
         
         return self.pos_drop(x)
     
-    def forward(self, x, prototype_stat=False):
+    def forward(self, x, supports_stat=False):
         x = self.prepare_tokens(x)
         for i, blk in enumerate(self.blocks):
-            if prototype_stat: # queries forward
-                assert self.prototype_stat[i] != None, 'prototype stat is empty'
-                x, mean1, var1, mean2, var2 = blk(x, prototype_stat = self.prototype_stat[i])
+            if supports_stat: # queries forward
+                assert self.supports_stat[i] != None, 'supports stat is empty'
+                x, mean1, var1, mean2, var2 = blk(x, supports_stat = self.supports_stat[i])
             else: # supports forward
                 x, mean1, var1, mean2, var2 = blk(x)
-                self.prototype_stat[i] = [mean1, var1, mean2, var2]
+                self.supports_stat[i] = [mean1, var1, mean2, var2]
                 
         x = self.norm(x)
         
         return x[:, 0]
+
 
 
 class BNTuning(Baseline):
@@ -102,10 +101,16 @@ class BNTuning(Baseline):
     def fix_encoder(self):
         for name, param in self.encoder.named_parameters():
             if 'norm' in name or 'st_' in name or 'bn' in name:
+            #if 'st_' in name or 'bn' in name:
                 print(name)
                 param.requires_grad = True
             else:
                 param.requires_grad = False
+            '''
+            if 'cls' in name and 'st' not in name:
+                print(name)
+                param.requires_grad = False
+            '''
     
     def load_backbone(self, patch_size=16, **kwargs):
         encoder =  VisionTransformer(
@@ -119,7 +124,7 @@ class BNTuning(Baseline):
         prototypes = torch.mean(x_support.view(args.train_num_ways, args.num_shots, -1), dim=1)
         prototypes = F.normalize(prototypes, dim=-1)
         
-        x_query = self.encoder(x_query, prototype_stat=True)
+        x_query = self.encoder(x_query, supports_stat=True)
         x_query = F.normalize(x_query, dim=-1)
         
         distance = torch.einsum('qd, wd -> qw', x_query, prototypes) # 75 5
