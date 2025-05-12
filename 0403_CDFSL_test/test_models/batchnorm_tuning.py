6b45d0e77c49f66560c11c36468e205ac0492fe4
zhaemin <95492807+zhaemin@users.noreply.github.com>
2025-05-12 14:58:02 +0900

    Add files via upload

--- 0403_CDFSL_test/test_models/batchnorm_tuning.py
+++ 0403_CDFSL_test/test_models/batchnorm_tuning.py
@@ -9,38 +9,70 @@ import random
 import numpy as np
 from functools import partial
 from tqdm import tqdm
+import kornia.augmentation as K
 
 from utils import split_support_query_set
 import backbone.vision_transformer as vit
 from test_models.baseline import Baseline
 
+def get_augmentation(name, input_shape):
+    if name == 'none':
+        return K.AugmentationSequential(
+            #return_transform=False, # `return_transform` is deprecated. Please access `.transform_matrix` in `AugmentationSequential` instead.
+            same_on_batch=False,
+        )
+
+    elif name == 'weak':
+        aug = K.AugmentationSequential(
+            K.RandomCrop(input_shape[1:], padding=4, padding_mode='reflect', resample='BICUBIC'),
+            K.RandomHorizontalFlip(),
+            #return_transform=False,
+            same_on_batch=False,
+        )
+        return aug
+
+    elif name == 'strong':
+        aug = K.AugmentationSequential(
+            K.RandomResizedCrop(input_shape[1:], scale=(0.08, 1.0), resample='BICUBIC'),
+            K.ColorJitter(0.4, 0.4, 0.4, 0.1, p=0.8),
+            K.RandomGrayscale(p=0.2),
+            K.RandomHorizontalFlip(),
+            #return_transform=False,
+            same_on_batch=False,
+        )
+        return aug
+
+    else:
+        raise Exception(f'Unknown Augmentation: {name}')
 
 class CustomBatchNorm(nn.Module):
     def __init__(self, eps=1e-6):
         super().__init__()
         self.eps = eps
         self.alpha = nn.Parameter(torch.Tensor([0.]))
-        #self.beta = nn.Parameter(torch.Tensor([0.]))
-
+        
     def forward(self, x, x_mean=None, x_var=None, weight=1., bias=0., alpha=0.5, beta=0.5):
         batch_mean, batch_var = None, None
         alpha = torch.sigmoid(self.alpha)
-        #beta = torch.sigmoid(self.beta)
+        #alpha = 1.0
+        
         # supports
-        if x_mean == None:
+        if x_mean == None and x_var == None:
             batch_mean = torch.mean(x, dim=(0,2), keepdim=True) # 1 N 1
             instance_mean = torch.mean(x, dim=-1, keepdim=True)
-            mean = alpha * batch_mean + (1 - alpha) * instance_mean
-        if x_var == None:
+            
             batch_var = torch.var(x, dim=(0,2), unbiased=False, keepdim=True)
             instance_var = torch.var(x, dim=-1, unbiased=False, keepdim=True)
+            
+            mean = alpha * batch_mean + (1 - alpha) * instance_mean
             var = alpha * batch_var + (1 - alpha) * instance_var + alpha * (1-alpha) * ((batch_mean-instance_mean)**2)
+        
         # queries
-        if x_mean != None:
+        if x_mean != None and x_var != None:
             instance_mean = torch.mean(x, dim=-1, keepdim=True)
-            mean = alpha * x_mean + (1 - alpha) * instance_mean
-        if x_var != None:
             instance_var = torch.var(x, dim=-1, unbiased=False, keepdim=True)
+            
+            mean = alpha * x_mean + (1 - alpha) * instance_mean
             var = alpha * x_var + (1 - alpha) * instance_var + alpha * (1-alpha) * ((x_mean-instance_mean)**2)
         
         x_norm = (x - mean) / ((var + self.eps) ** 0.5)
@@ -64,16 +96,16 @@ class BNBlock(vit.Block):
         mean1, var1, mean2, var2 = None, None, None, None
         if supports_stat != None:
             mean1, var1, mean2, var2 = supports_stat[0], supports_stat[1], supports_stat[2], supports_stat[3]
-
+            
         x_norm, mean1, var1 = self.st_bn1(x, x_mean=mean1, x_var=var1, weight=self.norm1.weight, bias=self.norm1.bias)
-        #x_norm = self.norm1(x)
         y, attn = self.attn(x_norm)
-        if return_attention:
-            return attn
         x = x + self.drop_path(y)
-        x_norm, mean2, var2 = self.st_bn2(x, x_mean=mean2, x_var=var2, weight=self.norm2.weight, bias=self.norm2.bias)
-        #x_norm = self.norm2(x)
+        
+        x_norm, mean2, var2 = self.st_bn2(x, x_mean=mean2, x_var=var2, weight=self.norm2.weight, bias=self.norm2.bias)        
         x = x + self.drop_path(self.mlp(x_norm))
+
+        if return_attention:
+            return x, mean1, var1, mean2, var2, attn
         
         return x, mean1, var1, mean2, var2
     
@@ -85,6 +117,7 @@ class VisionTransformer(vit.VisionTransformer):
         self.supports_stat = [None for _ in range(len(self.blocks)+1)]
         self.print_flag = True
         self.st_norm = CustomBatchNorm()
+        
     
     def prepare_tokens(self, x):
         B, nc, w, h = x.shape
@@ -102,6 +135,7 @@ class VisionTransformer(vit.VisionTransformer):
     
     def forward(self, x, supports_stat=False):
         x = self.prepare_tokens(x)
+        
         for i, blk in enumerate(self.blocks):
             if supports_stat: # queries forward
                 assert self.supports_stat[i] != None, 'supports stat is empty'
@@ -116,7 +150,7 @@ class VisionTransformer(vit.VisionTransformer):
             else: # supports forward
                 x, mean1, var1, mean2, var2 = blk(x)
                 self.supports_stat[i] = [mean1, var1, mean2, var2]
-        
+                
         if supports_stat: # queries
             x, mean, var = self.st_norm(x, x_mean=self.supports_stat[-1][0], x_var=self.supports_stat[-1][1], weight=self.norm.weight, bias=self.norm.bias)
         else: # supports
@@ -133,16 +167,18 @@ class BNTuning(Baseline):
         self.img_size = img_size
         self.patch_size = patch_size
         self.encoder = self.load_backbone(patch_size)
-        self.head = nn.Linear(self.encoder.num_features, 5)
         self.fix_encoder()
+
+        self.strong_aug = get_augmentation('strong', (3, 224, 224))
+        self.weak_aug = get_augmentation('weak', (3, 224, 224))
 
     def fix_encoder(self):
         for name, param in self.encoder.named_parameters():
             if 'norm' in name or 'st_' in name or 'bn' in name:
                 print(name)
                 param.requires_grad = True
-            else:
-                param.requires_grad = False
+                continue
+            param.requires_grad = False
             '''
             if 'cls' in name and 'st' not in name:
                 print(name)
@@ -176,9 +212,16 @@ class BNTuning(Baseline):
             distance = self.calculate_distance(args, x_support, x_query)
             logits = (distance / args.temperature).reshape(-1, args.test_num_ways)
             loss += F.cross_entropy(logits, y_query)
+
+            logits1 = self.calculate_distance(args, self.strong_aug(x_support), self.strong_aug(x_query))
+            logits2 = self.calculate_distance(args, self.weak_aug(x_support), self.weak_aug(x_query))
+            
+            aux_loss = (self.cross_entropyloss(logits1, logits2) + self.cross_entropyloss(logits2, logits1)) / 2
+            loss += aux_loss * 0.1
             
         return loss
     
+    '''
     def fewshot_acc(self, args, inputs, labels, device):
         with torch.no_grad():
             correct = 0
@@ -199,52 +242,66 @@ class BNTuning(Baseline):
                 
             acc = 100 * correct / total
         return acc
+    '''
     
-    def finetuning(self, loader, device, n_iters, args):
-        correct = 0
-        total = 0
-        loss = 0
+    def cross_entropyloss(self, logits1, logits2):
+        logits2 = logits2.detach()
+        p1_log = F.log_softmax(logits1 / 0.1, dim=1)
+        p2 = F.softmax(logits2 / 0.04, dim=1)
         
-        for data in tqdm(loader, desc="Test ..."):
-            inputs, labels = data
-            inputs, labels = inputs.to(device), labels.to(device)
-            tasks = split_support_query_set(inputs, labels, device, num_tasks=1, num_class=args.train_num_ways, num_shots=args.num_shots)
+        loss = - (p2 * p1_log).sum(dim=1).mean()
+        return loss
 
-            net = copy.deepcopy(self)
+    def fewshot_acc(self, args, inputs, labels, device):
+        tasks = split_support_query_set(inputs, labels, device, num_tasks=1, num_class=args.train_num_ways, num_shots=args.num_shots)
+        
+        net = copy.deepcopy(self)
+
+        norm_param = []
+        alpha_cls_param = []
+        for name, param in net.named_parameters():
+            if 'norm' in name:
+                norm_param.append(param)
+                continue
+            if 'alpha' in name and 'st_' in name:
+                alpha_cls_param.append(param)
+                continue
+        
+        optimizer = optim.AdamW([
+            {'params': norm_param, 'lr': 1e-6},
+            #{'params': alpha_cls_param, 'lr': 0.01},
+        ])
+        
+        correct = 0
+        loss = 0
+        total = 0
+        n_iters = 5
+        
+        for x_support, x_query, y_support, y_query in tasks:
             
-            ft_param = []
-            for name, param in net.named_parameters():
-                if 'alpha' in name or 'norm' in name or 'st_' in name:
-                    ft_param.append(param)
-                    
-            optimizer = optim.AdamW([
-                {'params': ft_param, 'lr': 0.01},
-                {'params': list(net.head.parameters()), 'lr': 0.01}
-            ])
-            loss = 0
+            net.train()
             
-            for x_support, x_query, y_support, y_query in tasks:
-                net.train()
-                for _ in range(n_iters):
-                    x_support_ = net.encoder(x_support)
-                    logits = net.head(x_support_)
-                    loss = F.cross_entropy(logits, y_support)
-                    
-                    optimizer.zero_grad()
-                    loss.backward()
-                    optimizer.step()
-                    
-                net.eval()
+            for i in range(n_iters):
                 
+                logits1 = net.calculate_distance(args, net.strong_aug(x_support), net.strong_aug(x_query))
+                logits2 = net.calculate_distance(args, net.weak_aug(x_support), net.weak_aug(x_query))
+                
+                loss = (net.cross_entropyloss(logits1, logits2) + net.cross_entropyloss(logits2, logits1)) / 2
+                
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+            
+            net.eval()
+            
+            
+            with torch.no_grad():
                 distance = net.calculate_distance(args, x_support, x_query)
                 logits = (distance / args.temperature).reshape(-1, args.test_num_ways)
                 _, predicted = torch.max(logits.data, 1)
                 correct += (predicted == y_query).sum().item()
                 total += y_query.size(0)
-            
-        for name, param in net.named_parameters():
-            if 'alpha' in name:
-                print(name, torch.sigmoid(param))
-        acc = 100 * correct / total
-            
+        
+            acc = 100 * correct / total
+            #print(acc)
         return acc
--- 0403_CDFSL_test/test_models/ttt.py
+++ 0403_CDFSL_test/test_models/ttt.py
@@ -8,10 +8,43 @@ import math
 import random
 import numpy as np
 from functools import partial
+import torchvision.transforms as transforms
+import kornia.augmentation as K
 
 from utils import split_support_query_set
 import backbone.vision_transformer as vit
 from test_models.baseline import Baseline
+
+def get_augmentation(name, input_shape):
+    if name == 'none':
+        return K.AugmentationSequential(
+            #return_transform=False, # `return_transform` is deprecated. Please access `.transform_matrix` in `AugmentationSequential` instead.
+            same_on_batch=False,
+        )
+
+    elif name == 'weak':
+        aug = K.AugmentationSequential(
+            K.RandomCrop(input_shape[1:], padding=4, padding_mode='reflect', resample='BICUBIC'),
+            K.RandomHorizontalFlip(),
+            #return_transform=False,
+            same_on_batch=False,
+        )
+        return aug
+
+    elif name == 'strong':
+        aug = K.AugmentationSequential(
+            K.RandomResizedCrop(input_shape[1:], scale=(0.08, 1.0), resample='BICUBIC'),
+            K.ColorJitter(0.4, 0.4, 0.4, 0.1, p=0.8),
+            K.RandomGrayscale(p=0.2),
+            K.RandomHorizontalFlip(),
+            #return_transform=False,
+            same_on_batch=False,
+        )
+        return aug
+
+    else:
+        raise Exception(f'Unknown Augmentation: {name}')
+
 
 class VisionTransformer(vit.VisionTransformer):
     def __init__(self, **kwargs):
@@ -20,10 +53,9 @@ class VisionTransformer(vit.VisionTransformer):
     def prepare_tokens(self, x, ntoken=None):
         B, nc, w, h = x.shape
         x = self.patch_embed(x)  # patch linear embedding
-        
-        cls_tokens = self.cls_token + ntoken
+            
         # add the [CLS] token to the embed patch tokens
-        cls_tokens = cls_tokens.expand(B, -1, -1)
+        cls_tokens = self.cls_token.expand(B, -1, -1)
         
         x = torch.cat((cls_tokens, x), dim=1)
 
@@ -34,11 +66,20 @@ class VisionTransformer(vit.VisionTransformer):
     
     def forward(self, x, ntoken=None):
         x = self.prepare_tokens(x, ntoken)
+        B = x.size(0)
+        if ntoken != None:
+            T = ntoken.size(1)
+            ntoken = ntoken.repeat(B, 1, 1)
+            x = torch.cat((ntoken, x), dim=1)
+            
         for blk in self.blocks:
             x = blk(x)
         x = self.norm(x)
         
+        if ntoken != None:
+            return x[:, :T].mean(dim=1)
         return x[:, 0]
+
 
 class TTTSSL(nn.Module):
     def __init__(self, img_size, patch_size):
@@ -48,6 +89,8 @@ class TTTSSL(nn.Module):
         self.encoder = self.load_backbone()
         self.ntoken = nn.Parameter(torch.zeros(1, 1, self.encoder.num_features))
         self.fix_encoder()
+        self.strong_aug = get_augmentation('strong', (3, 224, 224))
+        self.weak_aug = get_augmentation('weak', (3, 224, 224))
         
     def load_backbone(self, patch_size=16, **kwargs):
         encoder =  VisionTransformer(
@@ -63,25 +106,35 @@ class TTTSSL(nn.Module):
             else:
                 param.requires_grad = False
 
+    def inter_class_distance_loss(self, prototypes):
+        n_way = prototypes.size(0)
+        loss = 0
+        for i in range(n_way):
+            for j in range(i+1, n_way):
+                dist = F.pairwise_distance(prototypes[i].unsqueeze(0), prototypes[j].unsqueeze(0), p=2)
+                loss += -dist
+        return loss / (n_way * (n_way - 1) / 2)
+    
     def calculate_logits(self, args, x_support, x_query, ntoken):
         x_support = self.encoder(x_support, ntoken)
         prototypes = torch.mean(x_support.view(args.train_num_ways, args.num_shots, -1), dim=1)
         prototypes = F.normalize(prototypes, dim=-1)
+        inter_class_loss = self.inter_class_distance_loss(prototypes)
         
         x_query = self.encoder(x_query, ntoken)
         x_query = F.normalize(x_query, dim=-1)
         
         logits = torch.einsum('qd, wd -> qw', x_query, prototypes) # 75 5
         
-        return logits
+        return logits, inter_class_loss
     
     def forward(self, inputs, labels, args, device):
         tasks = split_support_query_set(inputs, labels, device, num_tasks=1, num_class=args.train_num_ways, num_shots=args.num_shots)
         
         total_loss = 0
         for x_support, x_query, y_support, y_query in tasks:
-            logits1 = self.calculate_logits(args, x_support[:, 0], x_query[:, 0], self.ntoken)
-            logits2 = self.calculate_logits(args, x_support[:, 1], x_query[:, 1], self.ntoken)
+            logits1 = self.calculate_logits(args, self.strong_aug(x_support), self.strong_aug(x_query), self.ntoken)
+            logits2 = self.calculate_logits(args, self.weak_aug(x_support), self.weak_aug(x_query), self.ntoken)
             
             loss = (self.cross_entropyloss(logits1, logits2) + self.cross_entropyloss(logits2, logits1)) / 2
             loss += (F.cross_entropy(logits1, y_query) + F.cross_entropy(logits2, y_query)) / 2
@@ -106,7 +159,7 @@ class TTTSSL(nn.Module):
         ntoken = copy.deepcopy(self.ntoken)
                 
         optimizer = optim.AdamW([
-            {'params': [ntoken], 'lr': 0.01},
+            {'params': [ntoken], 'lr': 1e-6},
         ])
         
         correct = 0
@@ -117,22 +170,27 @@ class TTTSSL(nn.Module):
         for x_support, x_query, y_support, y_query in tasks:
             self.train()
             
-            for _ in range(n_iters):
-                logits1 = self.calculate_logits(args, x_support[:, 0], x_query[:, 0], ntoken)
-                logits2 = self.calculate_logits(args, x_support[:, 1], x_query[:, 1], ntoken)
+            for i in range(n_iters):
+                logits1, inter_cls_loss1 = self.calculate_logits(args, self.strong_aug(x_support), self.strong_aug(x_query), ntoken)
+                logits2, inter_cls_loss2 = self.calculate_logits(args, self.weak_aug(x_support), self.weak_aug(x_query), ntoken)
                 
                 loss = (self.cross_entropyloss(logits1, logits2) + self.cross_entropyloss(logits2, logits1)) / 2
                 
+                if i == 0:
+                    first_loss = loss
+                elif i == n_iters-1:
+                    last_loss = loss
+                    print(first_loss - last_loss)
+                    
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
-            
+                
             self.eval()
             
             
             with torch.no_grad():
-                x_support, x_query = x_support[:, 2], x_query[:, 2]
-                distance = self.calculate_logits(args, x_support, x_query, ntoken)
+                distance, _ = self.calculate_logits(args, x_support, x_query, ntoken)
                 logits = (distance / args.temperature).reshape(-1, args.test_num_ways)
                 _, predicted = torch.max(logits.data, 1)
                 correct += (predicted == y_query).sum().item()
